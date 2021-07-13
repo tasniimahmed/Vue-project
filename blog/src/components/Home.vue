9895b991846cae239d9578b213a25e15718edc0d
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2021-07-13 10:48:55 +0200

    created a component

---
+++ blog/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <h1>Hello from Home Component</h1>
+    <h1>{{data}}</h1>
+    <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+   name: 'Home',
+   props: { 
+       data: String,
+       msg: String
+    }
+}
+</script>
