2ee19716920788f922d8b695d4dd0d923e03413d
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2021-07-13 11:08:17 +0200

    created an event

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -3,6 +3,7 @@
     <h1>Hello from Home Component</h1>
     <h1>{{data}}</h1>
     <h1>{{msg}}</h1>
+    <button v-on:mouseover="hello('dataa')">call hello</button>
     </div>
 </template>
 
@@ -13,6 +14,12 @@ export default {
    props: { 
        data: String,
        msg: String
+    },
+    methods:{
+        hello(item){
+            //note: events as: click, mouseover
+            alert(item)
+        }
     }
 }
 </script>
