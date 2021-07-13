7cab26b772ba375b5eada3a07492333c1dc4c08a
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2021-07-13 12:10:59 +0200

    looping

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -5,6 +5,16 @@
     <h1 v-if="show">{{msg}}</h1>
     <h1 v-else> Else part</h1>
     <button v-on:click="display()">Display</button>
+
+    <h1>looping</h1>
+    <table border="2px;">
+    <tr>
+        <td>ID</td><td>Name</td><td>Email</td>
+    </tr>
+    <tr v-for= "user in users" :key="user.id">
+        <td>{{user.id}}</td><td>{{user.name}}</td><td>{{user.email}}</td>
+    </tr>
+    </table>
     </div>
 </template>
 
@@ -29,7 +39,12 @@ export default {
     },
     data(){
         return{
-            show:false
+            show:false,
+            users:[
+                { id:1, name:'peter', email: 'peter@gmail.com'},
+                { id:2, name:'thomas', email: 'thomas@gmail.com'},
+                { id:3, name:'tasnim', email: 'tasnim@gmail.com'}
+            ]
         }
     }
 }
