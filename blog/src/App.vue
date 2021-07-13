51f9a2f2b03817bba390d7ca2a4c16f4b1054c69
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2021-07-13 14:37:35 +0200

    created a sign up form

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -3,19 +3,22 @@
     <img alt="Vue logo" src="./assets/logo.png">
    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
    <!-- <home data= "Tasnim is here" msg= "good"/> -->
-   <Login /> 
+   <!--<Login />--> 
+   <SignUp/>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 //import Home from './components/Home.vue'
-import Login from './components/Login.vue'
+//import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    Login
+   // Login,
+    SignUp
     //HelloWorld,
     //Home
   }
---
+++ blog/src/components/SignUp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="child">
+    <h1>Sign up Form</h1>
+    <input type="text" v-model= "signupForm.username" placeholder="Enter user name"/><br><br>
+    <input type="password" v-model= "signupForm.password" placeholder="Enter user password"/><br><br>
+    <p>Hobbies</p>
+    <input type="checkbox" v-model= "signupForm.hobbies" value="travel" id="travel" />
+    <label for="travel">Travel</label><br>
+    <input type="checkbox" v-model= "signupForm.hobbies" value="sports" id="sports" />
+    <label for="sports">Sports</label><br>
+    <button v-on:click = "signupuser() ">Sign Up</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+   name: 'SignUp',
+   data(){
+       return{
+           signupForm:{
+               username:null,
+               password:null,
+               hobbies:[]
+           }
+       }
+   },
+   methods:{
+       signupuser(){
+           console.log("signup is called", this.signupForm.hobbies)
+       }
+   }
+}
+</script>
+
+<style scoped>
+    .child{
+        background-color: rgb(163, 189, 206);
+        height: 500px;
+    }
+</style>
